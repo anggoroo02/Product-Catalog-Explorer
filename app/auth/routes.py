e4d4e141c9e18a20bf5e8a2299859fb6ff433db5
anggoroo02 <anggorojatipratikto004@gmail.com>
2026-07-17 00:30:16 +0700

    add method on auth; add box style to login register form

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,13 +1,19 @@
-from flask import render_template
-
+from flask import (
+    render_template,
+    redirect,
+    url_for,
+    flash
+)
 from app.auth import auth_bp
 from app.auth.forms import (
     LoginForm,
     RegisterForm
 )
+from app.extensions import db
+from app.models import User
 
 
-@auth_bp.route("/login")
+@auth_bp.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     return render_template(
@@ -15,7 +21,7 @@ def login():
         form=form
     )
 
-@auth_bp.route("/register")
+@auth_bp.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
     return render_template(
