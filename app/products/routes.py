ec0a43f0483ec9ff65962dfa7e0515c7d3e41b27
anggoroo02 <anggorojatipratikto004@gmail.com>
2026-07-17 04:44:52 +0700

    Integrate FakeStore API with Products route

--- app/products/routes.py
+++ app/products/routes.py
@@ -4,11 +4,18 @@ from flask_login import login_required
 
 from app.products import products_bp
 
+from app.services.product_service import (
+    get_all_products
+)
+
 
 @products_bp.route("/")
 @login_required
 def index():
 
+    products = get_all_products()
+
     return render_template(
-        "products/index.html"
+        "products/index.html",
+        products=products
     )
