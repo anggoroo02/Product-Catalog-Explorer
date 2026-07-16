87ba17718056f8ab6c61e4f8ed405f455ff078c7
anggoroo02 <anggorojatipratikto004@gmail.com>
2026-07-17 05:06:22 +0700

    add detail function

--- app/products/routes.py
+++ app/products/routes.py
@@ -4,18 +4,30 @@ from flask_login import login_required
 
 from app.products import products_bp
 
-from app.services.product_service import (
-    get_all_products
-)
+from app.services import product_service
 
 
-@products_bp.route("/")
+@products_bp.route("")
 @login_required
 def index():
 
-    products = get_all_products()
+    products = product_service.get_all_products()
 
     return render_template(
         "products/index.html",
         products=products
+    )
+
+
+@products_bp.route("/<int:product_id>")
+@login_required
+def detail(product_id):
+
+    product = product_service.get_product_by_id(
+        product_id
+    )
+
+    return render_template(
+        "products/detail.html",
+        product=product
     )
