6fa3e143327d1d822cd93db0c1ba624bc33c5178
anggoroo02 <anggorojatipratikto004@gmail.com>
2026-07-16 22:42:12 +0700

    Model User

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+from config import Config
+from app.extensions import db, login_manager
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Inisialisasi extension
+    from app import models
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # Route sementara untuk memastikan aplikasi berjalan
+    @app.route("/")
+    def index():
+        return """
+        <h1>Product Catalog Explorer</h1>
+        <p>Flask is Running!</p>
+        """
+
+    return app
